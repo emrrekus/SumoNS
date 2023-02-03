06febc86b2339928726ead789f569ba3096c1795
Emre <112270755+emrrekus@users.noreply.github.com>
2023-02-03 17:45:31 +0300

    Enemycontroller
    Enemycontroller

--- Assets/GameFolder/Scripts/Concretes/Controllers/EnemySpawnerController.cs
+++ Assets/GameFolder/Scripts/Concretes/Controllers/EnemySpawnerController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using SumoNS.Managers;
 using SumoNS.ScriptableObjects;
 using UnityEngine;
 
@@ -10,6 +12,15 @@ namespace SumoNS.Controllers
         [SerializeField] private SpawnInfoSO _spawnInfo;
 
 
+        private void Awake()
+        {
+            Spawn();
+        }
 
+        private void Spawn()
+        {
+           EnemyController enemyController= Instantiate(_spawnInfo.EnemyPrefab, transform.position, Quaternion.identity);
+           EnemyManager.Instance.AddEnemyController(enemyController);
+        }
     }
 }
--- Assets/GameFolder/Scripts/Concretes/Managers/EnemyManager.cs
+++ Assets/GameFolder/Scripts/Concretes/Managers/EnemyManager.cs
@@ -1,12 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using SumoNS.Abstracts.Utitiles;
+using SumoNS.Controllers;
 using UnityEngine;
 
 namespace SumoNS.Managers
 {
     public class EnemyManager : SingletonMBObject<EnemyManager>
     {
-       
+        [SerializeField] private List<EnemyController> _enemies;
+
+        public List<EnemyController> Enemies => _enemies;
+
+        private void Awake()
+        {
+            SingletonThisObject(this);
+            _enemies = new List<EnemyController>();
+        }
+
+        public void AddEnemyController(EnemyController enemyController)
+        {
+            enemyController.transform.parent = this.transform;
+            _enemies.Add(enemyController);
+        }
+        public void RemoveEnemyController(EnemyController enemyController)
+        {
+            _enemies.Remove(enemyController);
+        }
     }
 }
